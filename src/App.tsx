db835788bd8979d7c1d595d9adfdd148765a53c5
Akshay Singh <akshaysinghdev2@gmail.com>
2025-03-18 09:54:00 +0530

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,11 @@
-import CountBtn from "@/components/count-btn";
-import ReactSVG from "@/assets/react.svg";
-import { Badge } from "@/components/ui/badge";
+import { Button } from "./components/ui/button";
 
-function App() {
+const App = () => {
   return (
-    <main className="flex flex-col items-center justify-center h-screen">
-      <div className="flex flex-col items-center gap-y-4">
-        <div className="inline-flex items-center gap-x-4">
-          <img src={ReactSVG} alt="React Logo" className="w-32" />
-          <span className="text-6xl">+</span>
-          <img src={"/vite.svg"} alt="Vite Logo" className="w-32" />
-        </div>
-        <a
-          href="https://ui.shadcn.com"
-          rel="noopener noreferrer nofollow"
-          target="_blank"
-        >
-          <Badge variant="outline">shadcn/ui</Badge>
-        </a>
-        <CountBtn />
-      </div>
-    </main>
+    <div>
+      <Button>click me !!</Button>
+    </div>
   );
-}
+};
 
 export default App;
